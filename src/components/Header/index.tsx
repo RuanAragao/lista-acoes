22c73cf78e92447b4bc7a2175c7213a2ef57ef76
Ruan Aragão <ruanaragao2@gmail.com>
2023-08-03 00:44:43 -0300

    feat: add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,61 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 export function Header() {
   return (
-    <View>
-      <Text>
-        Header
-      </Text>
+    <View style={styles.header}>
+      <View style={styles.headerContent}>
+        <Text style={styles.headerTitle}>
+          AÇÕES
+        </Text>
+        <View style={styles.headerActions}>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => {}}
+          >
+            <Feather name="plus" size={24} color="#FFFFFF" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => {}}
+          >
+            <Feather name="refresh-cw" size={24} color="#FFFFFF" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => {}}
+          >
+            <Feather name="settings" size={24} color="#FFFFFF" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <StatusBar style="inverted" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header: {
     height: 128,
+    paddingTop: 44,
     backgroundColor: "#002E58"
+  },
+  headerContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginStart: 15,
+    marginEnd: 15,
+    marginTop: 24
+  },
+  headerTitle: {
+    fontSize: 36,
+    alignItems: "center",
+    color: "#FFFFFF"
+  },
+  headerActions: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8
   }
 });
--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View } from "react-native";
+import { Header } from "../../components/Header";
 
 export function List() {
   return (
     <View>
+      <Header />
       <Text>
         Lista de ações
       </Text>
