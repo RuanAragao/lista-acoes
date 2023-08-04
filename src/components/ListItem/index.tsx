4192a2d3b501dffcb733a12351990b54e6f8cac1
Ruan Aragão <ruanaragao2@gmail.com>
2023-08-04 17:04:26 -0300

    feat: quot list

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,21 +1,57 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
-export function ListItem() {
+interface Quote {
+  shortName: string;
+  regularMarketChange: number;
+  regularMarketChangePercent: number;
+  regularMarketTime: string;
+  regularMarketPrice: number;
+  symbol: string;
+}
+
+export function ListItem({ quote }: { quote: Quote }) {
+
+  function formatTime(isoDate: string): string {
+    const date = new Date(isoDate);
+
+    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  const regularMarketTime = formatTime(quote.regularMarketTime);
+
+  const regularChangesRender = () => {
+    if (quote.regularMarketChange < 0) {
+      return (
+        <Text style={styles.itemChangesNegative}>
+          {quote.regularMarketChange.toFixed(2)} ({quote.regularMarketChangePercent.toFixed(2)}%)
+        </Text>
+      )
+    }
+
+    return (
+      <Text style={styles.itemChanges}>
+        {quote.regularMarketChange.toFixed(2)} ({quote.regularMarketChangePercent.toFixed(2)})
+      </Text>
+    )
+  }
+
   return (
     <View style={styles.itemWrapper}>
       <View style={styles.itemFirstLine}>
-        <Text style={styles.itemName}>PETROBRAS PN N2</Text>
-        <Text style={styles.itemValue}>30.53</Text>
+        <Text style={styles.itemName}>{quote.shortName}</Text>
+        <Text style={styles.itemValue}>{quote.regularMarketPrice}</Text>
       </View>
       <View style={styles.itemSecondLine}>
         <View style={styles.itemTimeWrapper}>
           <Feather name="clock" size={16} color="#002E58" />
-          <Text style={styles.itemTime}>19:45:00</Text>
+          <Text style={styles.itemTime}>{regularMarketTime}</Text>
         </View>
-        <Text style={styles.itemChanges}>
-          -0.07 (-0.23%)
-        </Text>
+        { regularChangesRender() }
       </View>
     </View>
   );
@@ -65,6 +101,10 @@ const styles = StyleSheet.create({
   },
   itemChanges: {
     fontSize: 18,
-    color: "#002E58"
+    color: "#19B200"
+  },
+  itemChangesNegative: {
+    fontSize: 18,
+    color: "#B20000"
   }
 });
--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,14 +1,46 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import { Header } from "../../components/Header";
 import { ListItem } from "../../components/ListItem";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../config/axios";
 
 export function List() {
+  const ticketList = [
+    "PETR4",
+    "VALE3",
+    "ITUB4",
+    "BBDC4",
+    "ABEV3",
+    "BBAS3",
+    "PETR3",
+    "B3SA3",
+    "BDIV11",
+  ];
+  const ticketListString = ticketList.join("%2C");
+  const [quoteResponseList, setQuoteResponseList] = useState<string[]>([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/quote/${ticketListString}`)
+      .then((result) => {
+        if (!result["error"]) setQuoteResponseList(result.data.results);
+      })
+  }, []);
+
+  console.log(quoteResponseList)
+
+  const data = {
+    "regularMarketChange": -0.62999916,
+    "regularMarketChangePercent": -2.0375135,
+  }
   return (
     <View>
       <Header />
-      <ListItem />
-      <ListItem />
-      <ListItem />
+      <FlatList
+        data={quoteResponseList}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <ListItem key={item?.symbol} quote={item} />}
+      />
     </View>
   );
 }
